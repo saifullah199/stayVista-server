c9a312ab921fee0b450ad69fce7794bfd890b224
saifullah199 <mdkhalidsaifullah199@gmail.com>
2024-09-03 18:04:44 +0600

    second

--- index.js
+++ index.js
@@ -86,6 +86,15 @@ async function run() {
       res.send(result)
     })
 
+    //  Get a single room data from db using _id
+
+    app.get('/room/:id', async(req,res) => {
+      const id = req.params.id
+      const  query = {_id: new ObjectId(id)}
+      const result = await roomsCollection.findOne(query)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
